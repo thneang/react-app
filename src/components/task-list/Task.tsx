0d9a89c1778f5c96c03a6c6092b2943bd84b68d1
Thomas <thneang@gmail.com>
2025-04-25 17:44:42 +0200

    Refactoring field edition of Task component

--- src/components/task-list/Task.tsx
+++ src/components/task-list/Task.tsx
@@ -1,32 +1,14 @@
+import EditableDiv from "@/components/task-list/EditableDiv";
 import { useTaskContext } from "@/components/task-list/TaskContext";
 import { TaskType } from "@/types/global";
 import { useDraggable } from "@dnd-kit/core";
-import { useState } from "react";
 
 export interface TaskProps {
   task: TaskType;
 }
 
 export default function Task(props: TaskProps) {
-  const [localTitle, setLocalTitle] = useState(props.task.title);
-  const [localDescription, setLocalDescription] = useState(
-    props.task.description
-  );
   const taskContext = useTaskContext();
-
-  function handleTitleBlur() {
-    if (localTitle !== props.task.title) {
-      taskContext.handleUpdateTask(props.task.id, { title: localTitle });
-    }
-  }
-
-  function handleDescriptionBlur() {
-    if (localDescription !== props.task.description) {
-      taskContext.handleUpdateTask(props.task.id, {
-        description: localDescription,
-      });
-    }
-  }
 
   const { attributes, listeners, setNodeRef, transform } = useDraggable({
     id: props.task.id,
@@ -46,16 +28,20 @@ export default function Task(props: TaskProps) {
         style={dragStyle}
         className="flex flex-col border border-red-500 cursor-pointer"
       >
-        <textarea
-          value={localTitle}
-          onChange={(e) => setLocalTitle(e.target.value)}
-          onBlur={handleTitleBlur}
-        ></textarea>
-        <textarea
-          value={localDescription}
-          onChange={(e) => setLocalDescription(e.target.value)}
-          onBlur={handleDescriptionBlur}
-        ></textarea>
+        <EditableDiv
+          value={props.task.title}
+          onBlur={(e) =>
+            taskContext.handleUpdateTask(props.task.id, { title: e.target.value })
+          }
+        ></EditableDiv>
+        <EditableDiv
+          value={props.task.description}
+          onBlur={(e) =>
+            taskContext.handleUpdateTask(props.task.id, {
+              description: e.target.value
+            })
+          }
+        ></EditableDiv>
       </div>
     </>
   );
