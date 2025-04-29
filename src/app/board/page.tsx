d5beb196bb8190093b87f97d81188deee831279d
Thomas <thneang@gmail.com>
2025-04-30 01:45:14 +0200

    Refactoring done for loading data in the board page

--- src/app/board/page.tsx
+++ src/app/board/page.tsx
@@ -1,28 +1,12 @@
 "use client";
 import Board from "@/components/task-list/board/Board";
-import { StorageKey, useStorageInitialisation } from "@/lib/localstorage";
-import { ColumnList, TaskList } from "@/types/global";
-import {  useState } from "react";
+import {
+  useStorageInitialisation
+} from "@/lib/localstorage";
 
 export default function BoardPage() {
-  // Local state just to preload data for components
-  // The Board component will manage the state and interactivity
-  // This is just the drawback when the render depends on the local storage data
-  const [columns, setColumns] = useState<ColumnList>([]);
-  const [tasks, setTasks] = useState<TaskList>([]);
   
-  const defaultcolumns: ColumnList = [
-    { id: "1", name: "TODO" },
-    { id: "2", name: "In progress" },
-  ];
-  const defaultTaskList: TaskList = [
-    { id: "1", title: "title 1", description: "task 1", columnId: "1" },
-    { id: "2", title: "title 2", description: "task 2", columnId: "2" },
-    { id: "3", title: "title 3", description: "task 3", columnId: "1" },
-    { id: "4", title: "title 4", description: "task 4", columnId: "2" },
-  ];
-  useStorageInitialisation(StorageKey.COLUMNS, defaultcolumns, setColumns);
-  useStorageInitialisation(StorageKey.TASKS, defaultTaskList, setTasks);
+  const [columns, tasks] = useStorageInitialisation();
 
   if (!columns || !tasks) {
     return "Loading data...";
--- src/lib/localstorage.ts
+++ src/lib/localstorage.ts
@@ -1,4 +1,5 @@
-import { Dispatch, useEffect } from "react";
+import { ColumnList, TaskList } from "@/types/global";
+import { Dispatch, SetStateAction, useEffect, useState } from "react";
 
 export function saveToStorage(key: string, object: unknown) {
   if (object == undefined) {
@@ -15,29 +16,50 @@ export function getItemFromStorage(key: string) {
   return null;
 }
 
-export enum StorageKey{
+export enum StorageKey {
   COLUMNS = "columns",
-  TASKS = "tasks"
+  TASKS = "tasks",
 }
 
-export function useStorage(key: StorageKey, state: unknown) {
+export function useStorage<StateType>(key: StorageKey, state: StateType) {
   useEffect(() => {
     saveToStorage(key, state);
   }, [key, state]);
 }
 
-export function useStorageInitialisation<T>(
-  key: StorageKey,
-  initialData: T,
-  setState: Dispatch<React.SetStateAction<T>>
-) {
+const defaultcolumns: ColumnList = [
+  { id: "1", name: "TODO" },
+  { id: "2", name: "In progress" },
+];
+const defaultTaskList: TaskList = [
+  { id: "1", title: "title 1", description: "task 1", columnId: "1" },
+  { id: "2", title: "title 2", description: "task 2", columnId: "2" },
+  { id: "3", title: "title 3", description: "task 3", columnId: "1" },
+  { id: "4", title: "title 4", description: "task 4", columnId: "2" },
+];
+
+// Local state just to preload the initial data for components
+// The Board component will hold the real state and interactivity
+// useEffect usage is the drawback when the render depends on the local storage
+export function useStorageInitialisation() {
+  const [columns, setColumns] = useState<ColumnList>(defaultcolumns);
+  const [tasks, setTasks] = useState<TaskList>(defaultTaskList);
   useEffect(() => {
-    const data = getItemFromStorage(key);
-    if (data) {
-      setState(data);
-    } else {
-      saveToStorage(key, initialData);
-      setState(initialData);
+    function init<StateType>(
+      key: StorageKey,
+      defaultData: StateType,
+      setState: Dispatch<SetStateAction<StateType>>
+    ) {
+      const dataStored = getItemFromStorage(StorageKey.COLUMNS);
+      if (dataStored) {
+        setState(dataStored);
+      } else {
+        saveToStorage(key, defaultData);
+        setState(defaultData);
+      }
     }
-  }, [key, initialData, setState]);
+    init(StorageKey.COLUMNS, defaultcolumns, setColumns);
+    init(StorageKey.TASKS, defaultTaskList, setTasks);
+  }, []);
+  return [columns, tasks] as const;
 }
