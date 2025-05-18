2aa73385d22774aa6e13a2a9a616eeecdb6e73d6
Thomas <thneang@gmail.com>
2025-05-18 23:27:40 +0200

    trying other config

--- src/components/animations/Background.tsx
+++ src/components/animations/Background.tsx
@@ -4,12 +4,12 @@ import { useEffect, useMemo, useState } from "react";
 // import { loadAll } from "@tsparticles/all"; // if you are going to use `loadAll`, install the "@tsparticles/all" package too.
 // import { loadFull } from "tsparticles"; // if you are going to use `loadFull`, install the "tsparticles" package too.
 // import { loadSlim } from "@tsparticles/slim"; // if you are going to use `loadSlim`, install the "@tsparticles/slim" package too.
-import { basicOption } from "@/components/animations/ts-particles/basic";
+// import { basicOption } from "@/components/animations/ts-particles/basic";
 import { loadSlim } from "@tsparticles/slim"; // if you are going to use `loadBasic`, install the "@tsparticles/basic" package too.
+import { MoveDirection, OutMode } from "@tsparticles/engine";
 
 export function Background() {
   const [init, setInit] = useState(false);
-
   // this should be run only once per application lifetime
   useEffect(() => {
     initParticlesEngine(async (engine) => {
@@ -32,7 +32,76 @@ export function Background() {
   //   console.log(container);
   // };
 
-  const options = useMemo(() => basicOption, []);
+  const options = useMemo(
+    () => ({
+      background: {
+        color: {
+          value: "#000000",
+        },
+      },
+      fpsLimit: 60,
+      interactivity: {
+        events: {
+          onClick: {
+            enable: true,
+            mode: "push",
+          },
+          onHover: {
+            enable: true,
+            mode: "repulse",
+          },
+        },
+        modes: {
+          push: {
+            quantity: 4,
+          },
+          repulse: {
+            distance: 200,
+            duration: 0.4,
+          },
+        },
+      },
+      particles: {
+        color: {
+          value: "#ffffff",
+        },
+        links: {
+          color: "#ffffff",
+          distance: 150,
+          enable: true,
+          opacity: 0.5,
+          width: 1,
+        },
+        move: {
+          direction: MoveDirection.none,
+          enable: true,
+          outModes: {
+            default: OutMode.out,
+          },
+          random: false,
+          speed: 2,
+          straight: false,
+        },
+        number: {
+          density: {
+            enable: true,
+          },
+          value: 80,
+        },
+        opacity: {
+          value: 0.5,
+        },
+        shape: {
+          type: "circle",
+        },
+        size: {
+          value: { min: 1, max: 5 },
+        },
+      },
+      detectRetina: true,
+    }),
+    []
+  );
 
   if (init) {
     return (
