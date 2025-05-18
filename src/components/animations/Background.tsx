ec9c5d13e0f0d79d492067f0c8b4a41685e7297d
Thomas <thneang@gmail.com>
2025-05-18 23:36:44 +0200

    Trying config to bypass dark reader

--- src/components/animations/Background.tsx
+++ src/components/animations/Background.tsx
@@ -4,12 +4,12 @@ import { useEffect, useMemo, useState } from "react";
 // import { loadAll } from "@tsparticles/all"; // if you are going to use `loadAll`, install the "@tsparticles/all" package too.
 // import { loadFull } from "tsparticles"; // if you are going to use `loadFull`, install the "tsparticles" package too.
 // import { loadSlim } from "@tsparticles/slim"; // if you are going to use `loadSlim`, install the "@tsparticles/slim" package too.
-// import { basicOption } from "@/components/animations/ts-particles/basic";
+import { basicOption } from "@/components/animations/ts-particles/basic";
 import { loadSlim } from "@tsparticles/slim"; // if you are going to use `loadBasic`, install the "@tsparticles/basic" package too.
-import { MoveDirection, OutMode } from "@tsparticles/engine";
 
 export function Background() {
   const [init, setInit] = useState(false);
+
   // this should be run only once per application lifetime
   useEffect(() => {
     initParticlesEngine(async (engine) => {
@@ -18,9 +18,9 @@ export function Background() {
       // starting from v2 you can add only the features you need reducing the bundle size
       //await loadAll(engine);
       //await loadFull(engine);
-      // const isMobile = window.innerWidth < 768;
-      // const fps = isMobile ? 30 : 60;
-      // basicOption.fpsLimit = fps;
+      const isMobile = window.innerWidth < 768;
+      const fps = isMobile ? 30 : 60;
+      basicOption.fpsLimit = fps;
       await loadSlim(engine);
       // await loadBasic(engine);
     }).then(() => {
@@ -32,76 +32,7 @@ export function Background() {
   //   console.log(container);
   // };
 
-  const options = useMemo(
-    () => ({
-      background: {
-        color: {
-          value: "#000000",
-        },
-      },
-      fpsLimit: 60,
-      interactivity: {
-        events: {
-          onClick: {
-            enable: true,
-            mode: "push",
-          },
-          onHover: {
-            enable: true,
-            mode: "repulse",
-          },
-        },
-        modes: {
-          push: {
-            quantity: 4,
-          },
-          repulse: {
-            distance: 200,
-            duration: 0.4,
-          },
-        },
-      },
-      particles: {
-        color: {
-          value: "#ffffff",
-        },
-        links: {
-          color: "#ffffff",
-          distance: 150,
-          enable: true,
-          opacity: 0.5,
-          width: 1,
-        },
-        move: {
-          direction: MoveDirection.none,
-          enable: true,
-          outModes: {
-            default: OutMode.out,
-          },
-          random: false,
-          speed: 2,
-          straight: false,
-        },
-        number: {
-          density: {
-            enable: true,
-          },
-          value: 80,
-        },
-        opacity: {
-          value: 0.5,
-        },
-        shape: {
-          type: "circle",
-        },
-        size: {
-          value: { min: 1, max: 5 },
-        },
-      },
-      detectRetina: true,
-    }),
-    []
-  );
+  const options = useMemo(() => basicOption, []);
 
   if (init) {
     return (
@@ -110,6 +41,7 @@ export function Background() {
         // particlesLoaded={particlesLoaded}
         options={options}
         className={"fixed top-0 left-0 w-screen h-screen -z-10"}
+        data-darkreader-ignore
       />
     );
   }
